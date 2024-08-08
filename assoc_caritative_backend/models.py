f85f2b0999906c368dc977f2b24d58b33f44f30e
terasuamerto <erasuamert@gmail.com>
2024-08-08 11:22:44 +0200

    Second Commit

--- assoc_caritative_backend/models.py
+++ assoc_caritative_backend/models.py
@@ -2,4 +2,33 @@ from django.db import models
 
 # Create your models here.
 class Don(models.Model):
-    
+    intituleDon = models.CharField(max_length=200)
+    montantDon = models.FloatField()
+
+class Cagnotte(models.Model):
+    intituleCagnotte = models.CharField(max_length=200)
+    montantCagnotte = models.FloatField()
+    objectifMontantVise = models.FloatField()
+    dateDebut = models.DateField()
+    dateFin = models.DateField()
+
+class JustificationDon(models.Model):
+    motif = models.CharField(max_length=200)
+    montantJustification = models.FloatField()
+    infosBeneficiaire = models.TextField()
+
+
+class Utilisateur(models.Model):
+    nom = models.CharField(max_length=50)
+    prenom = models.CharField(max_length=50)
+    email = models.EmailField()
+    motdepasse = models.PasswordField()
+
+class Role(models.Model):
+    pass
+
+class Donateur(Utilisateur):
+    pass
+
+class Organisation(Utilisateur):
+    nomOrganisation = models.CharField(max_length=100)
