5bbe5c7130621b018d4a0d46868a2ed1dc6ac531
terasuamerto <erasuamert@gmail.com>
2024-08-09 04:15:35 +0000

    3rd Commit

--- assoc_caritative_backend/models.py
+++ assoc_caritative_backend/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Don(models.Model):
-    intituleDon = models.CharField(max_length=200)
-    montantDon = models.FloatField()
+class Utilisateur(models.Model):
+    nom = models.CharField(max_length=50)
+    prenom = models.CharField(max_length=50)
+    email = models.EmailField()
+    motdepasse = models.PasswordField()
 
 class Cagnotte(models.Model):
     intituleCagnotte = models.CharField(max_length=200)
@@ -11,24 +13,31 @@ class Cagnotte(models.Model):
     objectifMontantVise = models.FloatField()
     dateDebut = models.DateField()
     dateFin = models.DateField()
+    utilisateur = models.ForeignKey(Utilisateur)
+
+
+class Donateur(Utilisateur):
+    pass
+
+class Don(models.Model):
+    intituleDon = models.CharField(max_length=200)
+    montantDon = models.FloatField()
+    utilisateur = models.ForeignKey(Utilisateur)
+    donateur = models.ForeignKey(Donateur)
+    cagnote = models.ForeignKey(Cagnotte)
+
+class Organisation(Utilisateur):
+    nomOrganisation = models.CharField(max_length=100)
 
 class JustificationDon(models.Model):
     motif = models.CharField(max_length=200)
     montantJustification = models.FloatField()
     infosBeneficiaire = models.TextField()
+    cagnotte = models.ForeignKey(Cagnotte)
+    organisation = models.ForeignKey(Organisation)
 
-
-class Utilisateur(models.Model):
-    nom = models.CharField(max_length=50)
-    prenom = models.CharField(max_length=50)
-    email = models.EmailField()
-    motdepasse = models.PasswordField()
 
 class Role(models.Model):
     pass
 
-class Donateur(Utilisateur):
-    pass
 
-class Organisation(Utilisateur):
-    nomOrganisation = models.CharField(max_length=100)
